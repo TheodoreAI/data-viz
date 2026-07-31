5a8bbf7149b905129d6339594a37fa7d5c4a26f4
Mateo Estrada <mateoej12@gmail.com>
2026-07-31 14:46:36 -0700

    refactor: remove unused imports and functions; add wikidata module for art feed queries

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import random
-import re
 from datetime import date, timedelta
 from urllib.parse import quote
 
@@ -10,6 +9,7 @@ from flask import render_template
 from flask import request
 
 from vite import vite_asset_tags
+from wikidata import fetch_art_feed_page
 
 WIKIPEDIA_RANDOM_SUMMARY_URL = 'https://en.wikipedia.org/api/rest_v1/page/random/summary'
 WIKIPEDIA_SUMMARY_URL = 'https://en.wikipedia.org/api/rest_v1/page/summary/{title}'
@@ -21,28 +21,6 @@ WIKIPEDIA_TOP_VIEWED_URL = (
 WIKIPEDIA_HEADERS = {'User-Agent': 'data-viz-app/1.0 (mateoej12@gmail.com)'}
 EXCLUDED_TITLES = {'Main_Page', 'Special:Search'}
 BUBBLE_COUNT = 12
-
-WIKIDATA_SPARQL_URL = 'https://query.wikidata.org/sparql'
-ART_FEED_PAGE_SIZE = 20
-ART_FEED_QUERY = """
-SELECT ?painting ?paintingLabel ?artistLabel ?movementLabel
-       (SAMPLE(?birth) AS ?birthAgg)
-       (SAMPLE(?death) AS ?deathAgg)
-       (SAMPLE(?inception) AS ?inceptionAgg)
-       (SAMPLE(?image) AS ?imageAgg) WHERE {{
-  ?painting wdt:P31 wd:Q3305213;
-            wdt:P170 ?artist;
-            wdt:P18 ?image;
-            wdt:P135 ?movement;
-            wdt:P571 ?inception.
-  OPTIONAL {{ ?artist wdt:P569 ?birth. }}
-  OPTIONAL {{ ?artist wdt:P570 ?death. }}
-  SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}
-}}
-GROUP BY ?painting ?paintingLabel ?artistLabel ?movementLabel
-ORDER BY ?inceptionAgg
-LIMIT {limit} OFFSET {offset}
-"""
 
 TOPIC_CATEGORIES = {
     'art': 'Category:Art',
@@ -101,61 +79,6 @@ def fetch_random_article(topic=None):
     response = requests.get(WIKIPEDIA_RANDOM_SUMMARY_URL, headers=WIKIPEDIA_HEADERS)
     response.raise_for_status()
     return response.json()
-
-
-YEAR_PATTERN = re.compile(r'^(-?\d+)-\d{2}-\d{2}')
-
-
-def extract_year(iso_date):
-    if not iso_date:
-        return None
-    match = YEAR_PATTERN.match(iso_date)
-    if not match:
-        return None
-    try:
-        return int(match.group(1))
-    except ValueError:
-        return None
-
-
-def commons_thumbnail_url(file_path_url):
-    return file_path_url.replace('http://', 'https://') + '?width=600'
-
-
-QID_PATTERN = re.compile(r'^Q\d+$')
-
-
-def label_or_none(value):
-    if not value or value.startswith('http://www.wikidata.org/') or QID_PATTERN.match(value):
-        return None
-    return value
-
-
-def fetch_art_feed_page(offset, limit=ART_FEED_PAGE_SIZE):
-    query = ART_FEED_QUERY.format(limit=limit, offset=offset)
-    response = requests.get(
-        WIKIDATA_SPARQL_URL,
-        headers={**WIKIPEDIA_HEADERS, 'Accept': 'application/sparql-results+json'},
-        params={'query': query},
-    )
-    response.raise_for_status()
-    bindings = response.json()['results']['bindings']
-
-    paintings = []
-    for row in bindings:
-        image = row.get('imageAgg', {}).get('value')
-        if not image:
-            continue
-        paintings.append({
-            'title': label_or_none(row.get('paintingLabel', {}).get('value')) or 'Untitled',
-            'artist': label_or_none(row.get('artistLabel', {}).get('value')) or 'Unknown artist',
-            'birthYear': extract_year(row.get('birthAgg', {}).get('value')),
-            'deathYear': extract_year(row.get('deathAgg', {}).get('value')),
-            'movement': label_or_none(row.get('movementLabel', {}).get('value')) or 'Unknown movement',
-            'year': extract_year(row.get('inceptionAgg', {}).get('value')),
-            'image': commons_thumbnail_url(image),
-        })
-    return paintings
 
 
 @app.route('/')
